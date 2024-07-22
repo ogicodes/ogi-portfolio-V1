ea8d6b4be76ead646f0a779a7b2ddbbcd81e0a84
ogicodes <ogi@ogidesigns.ca>
2024-07-22 12:11:00 -0400

    created the light and dark modes for the website

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,25 @@
 import "./Hero.scss";
 import heroImg from "../../assets/pics/OGI-WHITE-BG.jpg";
-import themeIcon from "../../assets/icons/sun.svg";
-import githubIcon from "../../assets/icons/github-light.svg";
-import linkedinIcon from "../../assets/icons/linkedin-light.svg";
-import instagramIcon from "../../assets/icons/instagram-light.svg";
+import sun from "../../assets/icons/sun.svg";
+import moon from "../../assets/icons/moon.svg";
+import githubLight from "../../assets/icons/github-light.svg";
+import githubDark from "../../assets/icons/github-dark.svg";
+import linkedinLight from "../../assets/icons/linkedin-light.svg";
+import linkedinDark from "../../assets/icons/linkedin-dark.svg";
+import instagramLight from "../../assets/icons/instagram-light.svg";
+import instagramDark from "../../assets/icons/instagram-dark.svg";
 import CV from '../../assets/CV/Ognjen Dimitrijevic Resume.pdf'
+import { useTheme } from "../../common/ThemeContex";
 
 export default function Hero() {
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === "light" ? sun : moon;
+  const instagramIcon = theme === "light" ? instagramLight : instagramDark;
+  const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
+  const githubIcon = theme === "light" ? githubLight : githubDark;
+
+
   return (
     <section id="hero" className="hero">
       <div className="hero__color-mode-container">
@@ -15,9 +28,10 @@ export default function Hero() {
           className="hero__color-mode"
           src={themeIcon}
           alt="color mode icon"
+          onClick={toggleTheme}
         />
       </div>
-      <dic className="hero__info">
+      <div className="hero__info">
         <h1 className="hero__title">
           Ogi
           <br />
@@ -42,7 +56,7 @@ export default function Hero() {
         <a href={CV} download>
             <button className="hero__hover">Resume</button>
         </a>
-      </dic>
+      </div>
     </section>
   );
 }
